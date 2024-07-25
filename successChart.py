40de96882b62c716b8d46d620d32693181e8cec6
pthakur11205 <pthakur@uw.edu>
2024-07-25 14:10:27 -0400

    fixing graph display on flask framework

--- successChart.py
+++ successChart.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Read the CSV data into a DataFrame
-df = pd.read_csv('combined_draft_data_with_zScore.csv')
+df = pd.read_csv('combined_draft_data_with_percentiles.csv')
 
 # Convert columns to appropriate data types if necessary
 success_counts = df.groupby(['Team', 'Successful Pick']).size().unstack(fill_value=0)
@@ -10,18 +10,21 @@ success_counts = df.groupby(['Team', 'Successful Pick']).size().unstack(fill_val
 # Calculate percentage of successful picks
 success_counts['Success Rate %'] = success_counts[True] / (success_counts[True] + success_counts[False]) * 100
 
+# Sort the success rates in ascending order
+success_counts = success_counts.sort_values(by='Success Rate %')
+
 # Plot histogram
 plt.figure(figsize=(14, 8))
 success_counts['Success Rate %'].plot(kind='bar', stacked=False)
 plt.xlabel('Teams (Abbreviations)')
 plt.ylabel('Successful Pick Percentage (%)')
-plt.title('Successful Pick Percentage by Teams since 1989 (Z-Score method)')
+plt.title('Successful Pick Percentage by Teams since 1989 (Percentile method)')
 plt.xticks(rotation=45, ha='right')
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.tight_layout()
 
 # Save the graph as a PNG file
-plt.savefig('successful_pick_percentage_zScore.png')
+plt.savefig('static/images/successful_pick_percentage_percentile_sorted.png')
 
 # Show the plot
 plt.show()
